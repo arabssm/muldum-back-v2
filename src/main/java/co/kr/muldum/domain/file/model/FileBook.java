2defe7df5a4434d76dae9565cf24b2e254b39dd2
심현진 <hyunjin.dev143@gmail.com>
2025-08-03 21:58:35 +0900

    feat: 파일북 모델 클래스 추가

---
+++ src/main/java/co/kr/muldum/domain/file/model/FileBook.java
@@ -0,0 +1,34 @@
+package co.kr.muldum.domain.file.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Entity
+@Table(name = "file_books")
+public class FileBook {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "file_id", nullable = false)
+  private Long fileId;
+
+  @JdbcTypeCode(SqlTypes.JSON)
+  @Column(name = "book_config", columnDefinition = "jsonb")
+  private Map<String, Object> bookConfig;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+
+}
