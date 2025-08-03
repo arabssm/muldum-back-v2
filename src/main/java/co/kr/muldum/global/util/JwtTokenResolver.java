f174bec5655e5d55e11d58e55ffa93a2b87198de
randirao <randirao4245@gmail.com>
2025-08-03 12:23:32 +0900

    feat: HttpServletRequest에서 JWT 추출 및 인증 처리용 JwtTokenResolver 추가

---
+++ src/main/java/co/kr/muldum/global/util/JwtTokenResolver.java
@@ -0,0 +1,29 @@
+package co.kr.muldum.global.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.util.StringUtils;
+
+public class JwtTokenResolver {
+
+    private final JwtProvider jwtProvider;
+
+    public JwtTokenResolver(JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        return jwtProvider.validateToken(token);
+    }
+
+    public Object getAuthentication(String token) {
+        return jwtProvider.getAuthentication(token);
+    }
+}
