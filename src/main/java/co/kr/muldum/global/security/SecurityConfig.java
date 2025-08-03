71b22a464c8cf00345f7034a21daef54dd8d8aca
randirao <randirao4245@gmail.com>
2025-08-03 12:38:28 +0900

    feat: Import문 추가

--- src/main/java/co/kr/muldum/global/security/SecurityConfig.java
+++ src/main/java/co/kr/muldum/global/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package co.kr.muldum.global.security;
 
+import co.kr.muldum.global.util.JwtTokenResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,8 +9,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final JwtTokenResolver jwtTokenResolver;
+
+    public SecurityConfig(JwtTokenResolver jwtTokenResolver) {
+        this.jwtTokenResolver = jwtTokenResolver;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtTokenResolver);
+
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화
                 .authorizeHttpRequests(auth -> auth
