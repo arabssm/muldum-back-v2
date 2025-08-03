309ce91a8172e6498001b254a21d0a6a090f7ee7
randirao <randirao4245@gmail.com>
2025-08-03 17:09:19 +0900

    feat: 로그아웃 비즈니스 로직 구현

---
+++ src/main/java/co/kr/muldum/application/user/LogoutService.java
@@ -0,0 +1,26 @@
+package co.kr.muldum.application.user;
+
+import co.kr.muldum.domain.token.TokenRepository;
+import co.kr.muldum.global.exception.TokenNotFoundException;
+import co.kr.muldum.presentation.dto.LogoutRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService {
+
+    private final TokenRepository tokenRepository;
+
+    public void logout(LogoutRequestDto request) {
+        boolean deleted = tokenRepository.deleteByRefreshToken(
+                request.getUserType(),
+                request.getUserId(),
+                request.getRefreshToken()
+        );
+
+        if (!deleted) {
+            throw new TokenNotFoundException("유효한 리프레시 토큰이 없습니다.");
+        }
+    }
+}
