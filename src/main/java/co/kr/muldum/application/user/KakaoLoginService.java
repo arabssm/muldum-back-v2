0f9373d3941d122e998ee9b86b5f521ac7b4f748
randirao <randirao4245@gmail.com>
2025-08-04 19:50:51 +0900

    feat: 카카오 로그인 서비스 로직 구현

---
+++ src/main/java/co/kr/muldum/application/user/KakaoLoginService.java
@@ -0,0 +1,39 @@
+package co.kr.muldum.application.user;
+
+import co.kr.muldum.domain.user.UserReader;
+import co.kr.muldum.domain.user.model.Student;
+import co.kr.muldum.domain.user.model.UserInfo;
+import co.kr.muldum.domain.user.repository.StudentRepository;
+import co.kr.muldum.global.exception.CustomException;
+import co.kr.muldum.global.exception.ErrorCode;
+import co.kr.muldum.global.util.JwtProvider;
+import co.kr.muldum.infrastructure.user.oauth.KakaoOAuthClient;
+import co.kr.muldum.infrastructure.user.oauth.dto.KakaoUserInfoDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoLoginService {
+
+    private final KakaoOAuthClient kakaoOAuthClient;
+    private final StudentRepository studentRepository;
+    private final JwtProvider jwtProvider;
+    private final UserReader userReader;
+
+    public LoginResponseDto login(String accessToken) {
+        KakaoUserInfoDto userInfoDto = kakaoOAuthClient.getUserInfo(accessToken);
+
+        String email = userInfoDto.getEmail().trim();
+        System.out.println("카카오 로그인 이메일: " + email);
+
+        Student student = studentRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(ErrorCode.UNREGISTERED_USER));
+
+        String access = jwtProvider.createAccessToken(student.getId(), "STUDENT");
+        String refresh = jwtProvider.createRefreshToken(student.getId(), "STUDENT");
+        UserInfo userInfo = userReader.read(Student.class, student.getId());
+
+        return LoginResponseDto.of(userInfo, access, refresh);
+    }
+}
