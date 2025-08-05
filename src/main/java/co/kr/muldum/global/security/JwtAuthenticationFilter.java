5fe8bad10d19494f9bce1095ec4a29e4d42478bc
심현진 <hyunjin.dev143@gmail.com>
2025-08-05 14:29:16 +0900

    feat: JwtAuthenticationFilter에서 토큰 로깅 제거

--- src/main/java/co/kr/muldum/global/security/JwtAuthenticationFilter.java
+++ src/main/java/co/kr/muldum/global/security/JwtAuthenticationFilter.java
@@ -29,7 +29,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         // 1. 요청에서 토큰 꺼내기 (Authorization 헤더)
         String token = jwtTokenResolver.resolveToken(request);
-        log.info("[JwtFilter] token = {}", token);
         // 2. 토큰이 유효하다면, 인증 정보 설정하기
         if (token != null && jwtTokenResolver.validateToken(token)) {
             log.info("[JwtFilter] 유효한 토큰입니다.");
