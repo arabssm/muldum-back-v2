d11e62de7d14aa9f0c61efb7a1dd836428f89847
심현진 <hyunjin.dev143@gmail.com>
2025-08-05 16:33:55 +0900

    feat: 파일 타입 enum에 서브 디렉토리 경로 및 문자열 변환 메서드 추가

--- src/main/java/co/kr/muldum/domain/file/model/FileType.java
+++ src/main/java/co/kr/muldum/domain/file/model/FileType.java
@@ -1,7 +1,24 @@
 package co.kr.muldum.domain.file.model;
 
+import co.kr.muldum.domain.file.exception.InvalidFileTypeException;
+
 public enum FileType {
   NOTICE,
   BANNER,
-  TEAMSPACE
+  TEAMSPACE;
+  public String getUploadSubDir() {
+    return switch (this) {
+      case NOTICE -> "/notice";
+      case BANNER -> "/banner";
+      case TEAMSPACE -> "/teamspace";
+    };
+  }
+
+  public static FileType fromString(String type) {
+    try {
+      return FileType.valueOf(type.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      throw new InvalidFileTypeException("존재하지 않는 type입니다.");
+    }
+  }
 }
