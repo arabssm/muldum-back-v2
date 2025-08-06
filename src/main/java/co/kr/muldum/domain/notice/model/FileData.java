e2d5321c0555ff68e30c9a3bb2732b460cefb6d8
심현진 <hyunjin.dev143@gmail.com>
2025-08-07 00:45:42 +0900

    refactor: 불필요한 setter 제거

--- src/main/java/co/kr/muldum/domain/notice/model/FileData.java
+++ src/main/java/co/kr/muldum/domain/notice/model/FileData.java
@@ -4,11 +4,9 @@ import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Embeddable
 @Getter
-@Setter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class FileData {
