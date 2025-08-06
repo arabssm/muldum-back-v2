7457775f38366c999ea5a4830d39227d6ea02a9b
심현진 <hyunjin.dev143@gmail.com>
2025-08-07 00:12:59 +0900

    feat: Team 엔티티에 필드 추가 및 JSON 설정 적용

--- src/main/java/co/kr/muldum/domain/teamspace/model/Team.java
+++ src/main/java/co/kr/muldum/domain/teamspace/model/Team.java
@@ -1,17 +1,30 @@
 package co.kr.muldum.domain.teamspace.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Entity
 @Table(name = "teams")
 @Getter
 public class Team {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  // TO DO: erd에 따른 추가 필드 필요
-  // 예: 팀 이름, 설명, 생성일 등
+  @Column(nullable = false)
+  private String name;
+
+  @JdbcTypeCode(SqlTypes.JSON)
+  private Map<String, Object> config;
+
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at", nullable = true)
+  private LocalDateTime updatedAt;
 }
