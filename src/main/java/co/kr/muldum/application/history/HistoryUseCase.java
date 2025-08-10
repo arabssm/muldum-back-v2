c893dd1b28aac3e4e842235691601ab9c28d8a37
randirao <randirao4245@gmail.com>
2025-08-10 17:36:17 +0900

    feat: History 목록 조회 유스케이스 작성

---
+++ src/main/java/co/kr/muldum/application/history/HistoryUseCase.java
@@ -0,0 +1,24 @@
+package co.kr.muldum.application.history;
+
+import co.kr.muldum.application.history.dto.HistoryResponseDto;
+import co.kr.muldum.domain.history.model.History;
+import co.kr.muldum.domain.history.service.HistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class HistoryUseCase {
+
+    private final HistoryService historyService;
+
+    public List<HistoryResponseDto> getHistories(Integer generation) {
+        List<History> histories = historyService.findHistories(generation);
+        return histories.stream()
+                .map(HistoryResponseDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+}
