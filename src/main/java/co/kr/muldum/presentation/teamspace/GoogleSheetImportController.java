6376925afe38bf77c05725dfbedede430ba60868
심현진 <hyunjin.dev143@gmail.com>
2025-08-11 17:30:52 +0900

    fix: 학생 목록 및 등록된 학생 수 반환

--- src/main/java/co/kr/muldum/presentation/teamspace/GoogleSheetImportController.java
+++ src/main/java/co/kr/muldum/presentation/teamspace/GoogleSheetImportController.java
@@ -3,12 +3,15 @@ package co.kr.muldum.presentation.teamspace;
 import co.kr.muldum.application.teamspace.GoogleSheetImportService;
 import co.kr.muldum.application.teamspace.StudentCsvImportRequest;
 import co.kr.muldum.application.teamspace.StudentCsvImportResponse;
+import co.kr.muldum.domain.user.model.Student;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("tch/teamspace/invite")
@@ -20,7 +23,23 @@ public class GoogleSheetImportController {
   public ResponseEntity<StudentCsvImportResponse> importStudentsFromCsv(
           @RequestBody StudentCsvImportRequest studentCsvImportRequest
   ) {
-    googleSheetImportService.importFromGoogleSheet(studentCsvImportRequest.getGoogleSheetUrl());
-    return ResponseEntity.ok(new StudentCsvImportResponse());
+
+    List<Student> savedStudents = googleSheetImportService.importFromGoogleSheet(
+            studentCsvImportRequest.getGoogleSheetUrl()
+    );
+
+    List<StudentCsvImportResponse.StudentInfo> responseStudents = savedStudents.stream()
+            .map(s -> new StudentCsvImportResponse.StudentInfo(
+                    s.getProfile().get("name").toString(),
+                    s.getEmail()
+            ))
+            .toList();
+
+    StudentCsvImportResponse response = StudentCsvImportResponse.of(
+            savedStudents.size(),
+            responseStudents
+    );
+
+    return ResponseEntity.ok(response);
   }
 }
