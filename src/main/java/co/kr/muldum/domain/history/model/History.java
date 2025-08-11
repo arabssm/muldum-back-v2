5d74b78539bc916141b2509c491d437b20b24061
randirao <randirao4245@gmail.com>
2025-08-11 17:44:39 +0900

    feat: History 엔티티에 상세 조회를 위한 필드와 getter 메서드 추가

--- src/main/java/co/kr/muldum/domain/history/model/History.java
+++ src/main/java/co/kr/muldum/domain/history/model/History.java
@@ -31,9 +31,25 @@ public class History {
     @Column(name = "logo_url")
     private String logoUrl;
 
+    @Column
+    private String slogan;
+
+    @Column(columnDefinition = "text")
+    private String detailBackground;
+
+    @Column(columnDefinition = "text")
+    private String detailFeatures;
+
+    @Column(columnDefinition = "text")
+    private String detailResearch;
+
     @ElementCollection
     @CollectionTable(name = "history_awards", joinColumns = @JoinColumn(name = "history_id"))
     private List<HistoryAward> awards = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name = "history_contributors", joinColumns = @JoinColumn(name = "history_id"))
+    private List<Contributor> contributors = new ArrayList<>();
 
     @Getter
     @Setter
@@ -48,5 +64,18 @@ public class History {
 
         @Column(name = "given_at")
         private LocalDate givenAt;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Embeddable
+    public static class Contributor {
+
+        private String name;
+
+        private String githubUrl;
     }
 }
