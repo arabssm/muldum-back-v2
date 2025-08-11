673bb78a005d4ec50610ff2b4769d2c9bc3e2c74
심현진 <hyunjin.dev143@gmail.com>
2025-08-11 23:12:01 +0900

    feat: 공지 수정 메서드 추가 및 유효성 검사 로직 구현

--- src/main/java/co/kr/muldum/domain/notice/model/Notice.java
+++ src/main/java/co/kr/muldum/domain/notice/model/Notice.java
@@ -1,5 +1,6 @@
 package co.kr.muldum.domain.notice.model;
 
+import co.kr.muldum.application.notice.command.CreateNoticeRequest;
 import co.kr.muldum.domain.notice.model.enums.Status;
 import co.kr.muldum.domain.user.model.Teacher;
 import jakarta.persistence.*;
@@ -12,6 +13,8 @@ import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "notices")
@@ -48,4 +51,39 @@ public class Notice {
   @Builder.Default
   @Column(name = "updated_at", nullable = false)
   private LocalDateTime updatedAt = LocalDateTime.now();
+
+  public void updateNotice(CreateNoticeRequest createNoticeRequest) {
+    validateRequest(createNoticeRequest);
+
+    this.title = createNoticeRequest.getTitle();
+    this.contentData = new ContentData(
+        createNoticeRequest.getContent(),
+        createNoticeRequest.getFiles() == null ? List.of() :
+            createNoticeRequest.getFiles().stream()
+                    .filter(Objects::nonNull)
+                    .map(file -> new FileData(file.getUrl()))
+                    .toList()
+    );
+    this.status = createNoticeRequest.getStatus();
+    this.deadlineDate = createNoticeRequest.getDeadlineDate();
+  }
+
+  private void validateRequest(CreateNoticeRequest request) {
+    if (request.getTitle() == null || request.getTitle().isBlank()) {
+      throw new IllegalArgumentException("공지 제목은 필수입니다.");
+    }
+    if (request.getStatus() == null) {
+      throw new IllegalArgumentException("공지 상태는 필수입니다.");
+    }
+  }
+
+  @PrePersist
+  public void onCreate() {
+    this.updatedAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  public void onUpdate() {
+    this.updatedAt = LocalDateTime.now();
+  }
 }
