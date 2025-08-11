0cd0f981f53ef7fe86f813c9b44b982059ac4903
심현진 <hyunjin.dev143@gmail.com>
2025-08-11 17:35:37 +0900

    chore: 카카오 로그인 관련 로직 삭제

--- src/main/java/co/kr/muldum/presentation/user/AuthController.java
+++ src/main/java/co/kr/muldum/presentation/user/AuthController.java
@@ -1,10 +1,8 @@
 package co.kr.muldum.presentation.user;
 
 import co.kr.muldum.application.auth.TokenRefreshService;
-import co.kr.muldum.application.user.KakaoLoginService;
 import co.kr.muldum.application.user.LoginResponseDto;
 import co.kr.muldum.application.user.OAuthLoginService;
-import co.kr.muldum.global.dto.KakaoLoginRequestDto;
 import co.kr.muldum.global.dto.MessageResponse;
 import co.kr.muldum.global.dto.TokenRefreshRequestDto;
 import co.kr.muldum.infrastructure.user.oauth.dto.GoogleLoginRequestDto;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final OAuthLoginService oAuthLoginService;
-    private final KakaoLoginService kakaoLoginService;
     private final TokenRefreshService tokenRefreshService;
 
     @PostMapping("/login/google")
@@ -27,13 +24,6 @@ public class AuthController {
     ) {
         LoginResponseDto response = oAuthLoginService.loginWithGoogle(request.getAccessToken());
         return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/auth/login/kakao")
-    public LoginResponseDto kakaoLogin(
-            @RequestBody KakaoLoginRequestDto requestDto
-    ) {
-        return kakaoLoginService.login(requestDto.getAccessToken());
     }
 
     @PostMapping("/refresh")
