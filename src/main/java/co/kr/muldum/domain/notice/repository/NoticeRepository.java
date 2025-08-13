6a856a4684d4584dc9f6344030c290560d04e133
심현진 <hyunjin.dev143@gmail.com>
2025-08-13 15:49:59 +0900

    feat: 공지 목록 조회를 위한 최신 업데이트 순 정렬 쿼리 추가

--- src/main/java/co/kr/muldum/domain/notice/repository/NoticeRepository.java
+++ src/main/java/co/kr/muldum/domain/notice/repository/NoticeRepository.java
@@ -1,7 +1,14 @@
 package co.kr.muldum.domain.notice.repository;
 
 import co.kr.muldum.domain.notice.model.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+
+  @Query("SELECT n FROM Notice n " +
+          "ORDER BY COALESCE(n.updatedAt) DESC")
+  Page<Notice> getAllOrderByUpdatedAt(Pageable pageable);
 }
