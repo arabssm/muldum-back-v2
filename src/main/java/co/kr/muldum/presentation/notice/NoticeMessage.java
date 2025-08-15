52473816c5be4ab77fcdcd1886d5fc02b5941ffe
심현진 <hyunjin.dev143@gmail.com>
2025-08-15 21:32:22 +0900

    fix: 공지사항 수정 성공 메시지의 문법 오류 수정

--- src/main/java/co/kr/muldum/presentation/notice/NoticeMessage.java
+++ src/main/java/co/kr/muldum/presentation/notice/NoticeMessage.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum NoticeMessage {
   NOTICE_CREATED_SUCCESS("공지사항이 성공적으로 등록되었습니다."),
   NOTICE_DELETED_SUCCESS("공지사항이 성공적으로 삭제되었습니다."),
-  NOTICE_UPDATED_SUCCESS("공지사항이 성공적으로 수정되었습니다."),;
+  NOTICE_UPDATED_SUCCESS("공지사항이 성공적으로 수정되었습니다.");
 
   private final String message;
 
