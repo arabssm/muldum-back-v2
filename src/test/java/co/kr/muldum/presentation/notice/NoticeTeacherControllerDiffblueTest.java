342d1dd50ece531e670b65482e8b4c49d91ecaae
심현진 <hyunjin.dev143@gmail.com>
2025-08-15 21:55:41 +0900

    test: 공지 수정 및 삭제 유닛 테스트 코드 추가

--- src/test/java/co/kr/muldum/presentation/notice/NoticeTeacherControllerDiffblueTest.java
+++ src/test/java/co/kr/muldum/presentation/notice/NoticeTeacherControllerDiffblueTest.java
@@ -1,7 +1,6 @@
 package co.kr.muldum.presentation.notice;
 
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
 
 import co.kr.muldum.application.notice.command.CreateNoticeRequest;
 import co.kr.muldum.application.notice.command.NoticeCommandService;
@@ -9,11 +8,14 @@ import co.kr.muldum.global.exception.GlobalExceptionHandler;
 import co.kr.muldum.global.security.CustomUserDetails;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.aot.DisabledInAotMode;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -21,6 +23,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @ContextConfiguration(classes = {NoticeTeacherController.class, GlobalExceptionHandler.class})
@@ -37,6 +40,34 @@ class NoticeTeacherControllerDiffblueTest {
   private NoticeTeacherController noticeTeacherController;
 
   /**
+   * Test {@link NoticeTeacherController#updateNotice(Long, CreateNoticeRequest,
+   * CustomUserDetails)}.
+   *
+   * <p>Method under test: {@link NoticeTeacherController#updateNotice(Long, CreateNoticeRequest,
+   * CustomUserDetails)}
+   */
+  @Test
+  @DisplayName("Test updateNotice(Long, CreateNoticeRequest, CustomUserDetails)")
+  @Tag("ContributionFromDiffblue")
+  void testUpdateNotice() throws Exception {
+    // Arrange
+    MockHttpServletRequestBuilder contentTypeResult =
+            MockMvcRequestBuilders.patch("/tch/notice/{notice_id}", 1L)
+                    .contentType(MediaType.APPLICATION_JSON);
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    MockHttpServletRequestBuilder requestBuilder =
+            contentTypeResult.content(objectMapper.writeValueAsString(new CreateNoticeRequest()));
+
+    // Act and Assert
+    MockMvcBuilders.standaloneSetup(noticeTeacherController)
+            .setControllerAdvice(globalExceptionHandler)
+            .build()
+            .perform(requestBuilder)
+            .andExpect(MockMvcResultMatchers.status().is(400));
+  }
+
+  /**
    * Test {@link NoticeTeacherController#createNotice(CreateNoticeRequest, CustomUserDetails)}.
    *
    * <p>Method under test: {@link NoticeTeacherController#createNotice(CreateNoticeRequest,
@@ -44,10 +75,9 @@ class NoticeTeacherControllerDiffblueTest {
    */
   @Test
   @DisplayName("Test createNotice(CreateNoticeRequest, CustomUserDetails)")
+  @Tag("ContributionFromDiffblue")
   void testCreateNotice() throws Exception {
     // Arrange
-    when(noticeCommandService.createNotice(Mockito.<CreateNoticeRequest>any(), Mockito.<Long>any()))
-            .thenReturn(1L);
     MockHttpServletRequestBuilder contentTypeResult =
             MockMvcRequestBuilders.post("/tch/notice").contentType(MediaType.APPLICATION_JSON);
     ObjectMapper objectMapper = new ObjectMapper();
@@ -59,21 +89,79 @@ class NoticeTeacherControllerDiffblueTest {
             .setControllerAdvice(globalExceptionHandler)
             .build()
             .perform(requestBuilder)
-            .andExpect(MockMvcResultMatchers.status().isCreated())
-            .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-            .andExpect(
-                    MockMvcResultMatchers.content()
-                            .string("{\"id\":1,\"message\":\"공지사항이 성공적으로 등록되었습니다.\"}"));
+            .andExpect(MockMvcResultMatchers.status().is(400));
   }
 
   /**
    * Test {@link NoticeTeacherController#deleteNotice(Long, CustomUserDetails)}.
    *
+   * <ul>
+   *   <li>Given one.
+   *   <li>Then status four hundred.
+   * </ul>
+   *
    * <p>Method under test: {@link NoticeTeacherController#deleteNotice(Long, CustomUserDetails)}
    */
   @Test
-  @DisplayName("Test deleteNotice(Long, CustomUserDetails)")
-  void testDeleteNotice() throws Exception {
+  @DisplayName("Test deleteNotice(Long, CustomUserDetails); given one; then status four hundred")
+  @Tag("ContributionFromDiffblue")
+  void testDeleteNotice_givenOne_thenStatusFourHundred() throws Exception {
+    // Arrange
+    doNothing().when(noticeCommandService).deleteNotice(Mockito.<Long>any(), Mockito.<Long>any());
+    MockHttpServletRequestBuilder requestBuilder =
+            MockMvcRequestBuilders.delete("/tch/notice/{notice_id}", "Uri Variables", "Uri Variables");
+
+    // Act and Assert
+    MockMvcBuilders.standaloneSetup(noticeTeacherController)
+            .setControllerAdvice(globalExceptionHandler)
+            .build()
+            .perform(requestBuilder)
+            .andExpect(MockMvcResultMatchers.status().is(400));
+  }
+
+  /**
+   * Test {@link NoticeTeacherController#deleteNotice(Long, CustomUserDetails)}.
+   *
+   * <ul>
+   *   <li>Given {@code /tch/notice/{notice_id}}.
+   *   <li>When formLogin.
+   *   <li>Then status {@link StatusResultMatchers#isNotFound()}.
+   * </ul>
+   *
+   * <p>Method under test: {@link NoticeTeacherController#deleteNotice(Long, CustomUserDetails)}
+   */
+  @Test
+  @DisplayName(
+          "Test deleteNotice(Long, CustomUserDetails); given '/tch/notice/{notice_id}'; when formLogin; then status isNotFound()")
+  @Tag("ContributionFromDiffblue")
+  void testDeleteNotice_givenTchNoticeNoticeId_whenFormLogin_thenStatusIsNotFound()
+          throws Exception {
+    // Arrange
+    doNothing().when(noticeCommandService).deleteNotice(Mockito.<Long>any(), Mockito.<Long>any());
+    FormLoginRequestBuilder requestBuilder = SecurityMockMvcRequestBuilders.formLogin();
+
+    // Act and Assert
+    MockMvcBuilders.standaloneSetup(noticeTeacherController)
+            .setControllerAdvice(globalExceptionHandler)
+            .build()
+            .perform(requestBuilder)
+            .andExpect(MockMvcResultMatchers.status().isNotFound());
+  }
+
+  /**
+   * Test {@link NoticeTeacherController#deleteNotice(Long, CustomUserDetails)}.
+   *
+   * <ul>
+   *   <li>When one.
+   *   <li>Then status {@link StatusResultMatchers#isOk()}.
+   * </ul>
+   *
+   * <p>Method under test: {@link NoticeTeacherController#deleteNotice(Long, CustomUserDetails)}
+   */
+  @Test
+  @DisplayName("Test deleteNotice(Long, CustomUserDetails); when one; then status isOk()")
+  @Tag("ContributionFromDiffblue")
+  void testDeleteNotice_whenOne_thenStatusIsOk() throws Exception {
     // Arrange
     doNothing().when(noticeCommandService).deleteNotice(Mockito.<Long>any(), Mockito.<Long>any());
     MockHttpServletRequestBuilder requestBuilder =
@@ -84,8 +172,9 @@ class NoticeTeacherControllerDiffblueTest {
             .setControllerAdvice(globalExceptionHandler)
             .build()
             .perform(requestBuilder)
-            .andExpect(MockMvcResultMatchers.status().isNoContent())
-            .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"))
-            .andExpect(MockMvcResultMatchers.content().string("????? ????? ???????."));
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+            .andExpect(
+                    MockMvcResultMatchers.content().string("{\"message\":\"공지사항이 성공적으로 삭제되었습니다.\"}"));
   }
 }
