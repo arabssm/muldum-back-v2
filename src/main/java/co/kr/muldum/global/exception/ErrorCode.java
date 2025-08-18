0c4c10cf07a38283218859e827872f536ab5b635
randirao <randirao4245@gmail.com>
2025-08-18 21:32:58 +0900

    feat: 구글 로그인 관련 에러 코드 추가

--- src/main/java/co/kr/muldum/global/exception/ErrorCode.java
+++ src/main/java/co/kr/muldum/global/exception/ErrorCode.java
@@ -7,7 +7,8 @@ public enum ErrorCode {
     UNAUTHORIZED_DOMAIN(HttpStatus.UNAUTHORIZED, "허용되지 않은 이메일 도메인입니다."),
     UNREGISTERED_USER(HttpStatus.UNAUTHORIZED, "등록되지 않은 사용자입니다."),
     GOOGLE_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "구글 액세스 토큰이 만료되었습니다."),
-    GOOGLE_API_ERROR(HttpStatus.BAD_GATEWAY, "구글 API 호출 중 오류가 발생했습니다.");
+    GOOGLE_API_ERROR(HttpStatus.BAD_GATEWAY, "구글 API 호출 중 오류가 발생했습니다."),
+    EXTERNAL_PROVIDER_ERROR(HttpStatus.BAD_GATEWAY, "외부 인증 서버 오류");
 
     private final HttpStatus status;
     private final String message;
