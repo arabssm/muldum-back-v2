cdcdf2896212e10128e4b8c7033b3307ad280c77
심현진 <hyunjin.dev143@gmail.com>
2025-08-19 12:26:08 +0900

    feat: NoticeSimpleResponse를 레코드로 변경하고 teacher 필드 추가

--- src/main/java/co/kr/muldum/application/notice/query/NoticeSimpleResponse.java
+++ src/main/java/co/kr/muldum/application/notice/query/NoticeSimpleResponse.java
@@ -1,28 +1,21 @@
 package co.kr.muldum.application.notice.query;
 
 import co.kr.muldum.domain.notice.model.Notice;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
-public class NoticeSimpleResponse {
-  private final Long id;
-  private final String title;
-  private final LocalDateTime updatedAt;
-  private Long authorUserId;
-
-  public NoticeSimpleResponse(Long id, String title, LocalDateTime updatedAt, Long authorUserId) {
-    this.id = id;
-    this.title = title;
-    this.updatedAt = updatedAt;
-  }
+public record NoticeSimpleResponse(
+        Long id,
+        String title,
+        LocalDateTime updatedAt,
+        String teacher
+) {
   public static NoticeSimpleResponse fromEntity(Notice notice) {
     return new NoticeSimpleResponse(
-        notice.getId(),
-        notice.getTitle(),
-        notice.getUpdatedAt(),
-        notice.getTeacher().getId()
+            notice.getId(),
+            notice.getTitle(),
+            notice.getUpdatedAt(),
+            notice.getTeacher().getName()
     );
   }
 }
