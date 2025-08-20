2c5a94787bc313ee67a7f70d2545936c03d2f7dc
심현진 <hyunjin.dev143@gmail.com>
2025-08-20 10:27:43 +0900

    feat: 팀 ID에 따른 공지사항 조회 및 일반 공지 조회 기능 추가

--- src/main/java/co/kr/muldum/domain/notice/repository/NoticeRepository.java
+++ src/main/java/co/kr/muldum/domain/notice/repository/NoticeRepository.java
@@ -1,14 +1,26 @@
 package co.kr.muldum.domain.notice.repository;
 
 import co.kr.muldum.domain.notice.model.Notice;
+import co.kr.muldum.domain.notice.model.enums.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
   @Query("SELECT n FROM Notice n " +
           "ORDER BY COALESCE(n.updatedAt) DESC")
   Page<Notice> getAllOrderByUpdatedAt(Pageable pageable);
+
+  @Query("select n from Notice n " +
+          "where n.status = :status " +
+          "ORDER BY COALESCE(n.updatedAt) DESC")
+  Page<Notice> findGeneralNotice(@Param("status") Status status, Pageable pageable);
+
+  @Query("SELECT nt FROM NoticeTeam nt JOIN nt.notice n " +
+          "WHERE nt.team = :teamId " +
+          "ORDER BY COALESCE(n.updatedAt) DESC")
+  Page<Notice> findAllNoticeByTeamId(Long teamId, Pageable pageable);
 }
