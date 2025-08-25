935a14bd512ce74b8a94ed38bcee9301a3340db6
심현진 <hyunjin.dev143@gmail.com>
2025-08-26 08:18:44 +0900

    refactor: getAllNotices 인증 로직 및 팀 관련 로직 삭제

--- src/main/java/co/kr/muldum/application/notice/query/NoticeQueryService.java
+++ src/main/java/co/kr/muldum/application/notice/query/NoticeQueryService.java
@@ -1,37 +1,18 @@
 package co.kr.muldum.application.notice.query;
 
-import co.kr.muldum.domain.notice.model.enums.Status;
 import co.kr.muldum.domain.notice.repository.NoticeRepository;
-import co.kr.muldum.domain.user.repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import org.springframework.security.access.AccessDeniedException;
-
 @Service
 @RequiredArgsConstructor
 public class NoticeQueryService {
   private final NoticeRepository noticeRepository;
-  private final TeacherRepository teacherRepository;
 
-  public Page<NoticeSimpleResponse> getAllNotices(Pageable pageable, Long userId) {
-    boolean isTeacher = teacherRepository.existsById(userId);
-    if(!isTeacher){
-      throw new AccessDeniedException("해당 사용자는 교사가 아닙니다.");
-    }
-
-    return noticeRepository.getAllOrderByUpdatedAt(pageable)
-            .map(NoticeSimpleResponse::fromEntity);
-  }
-
-  public Page<NoticeSimpleResponse> getAllNoticesByTeamId(Pageable pageable, Long teamId) {
-    if(teamId == null){
-      return noticeRepository.findGeneralNotice(Status.GENERAL, pageable)
-              .map(NoticeSimpleResponse::fromEntity);
-    }
-    return noticeRepository.findAllNoticeByTeamId(teamId, pageable)
+  public Page<NoticeSimpleResponse> getAllNotices(Pageable pageable) {
+    return noticeRepository.findAllNotice(pageable)
             .map(NoticeSimpleResponse::fromEntity);
   }
 }
