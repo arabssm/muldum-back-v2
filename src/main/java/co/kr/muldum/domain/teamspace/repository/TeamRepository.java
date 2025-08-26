5acb561252de545642f360e66297dcd5336132d8
randirao <randirao4245@gmail.com>
2025-08-26 12:21:51 +0900

    feat: TeamRepository findByName 메서드 추가

--- src/main/java/co/kr/muldum/domain/teamspace/repository/TeamRepository.java
+++ src/main/java/co/kr/muldum/domain/teamspace/repository/TeamRepository.java
@@ -2,6 +2,8 @@ package co.kr.muldum.domain.teamspace.repository;
 
 import co.kr.muldum.domain.teamspace.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    Optional<Team> findByName(String name);
 }
