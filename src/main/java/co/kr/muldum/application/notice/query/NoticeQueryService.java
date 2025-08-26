583eeba358ebd490658146ed19614f7a3b697249
심현진 <hyunjin.dev143@gmail.com>
2025-08-26 14:17:57 +0900

    feat: 공지 단건 조회 상세 정보 반환 기능 추가

--- src/main/java/co/kr/muldum/application/notice/query/NoticeQueryService.java
+++ src/main/java/co/kr/muldum/application/notice/query/NoticeQueryService.java
@@ -1,6 +1,7 @@
 package co.kr.muldum.application.notice.query;
 
 import co.kr.muldum.domain.notice.repository.NoticeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,5 +15,11 @@ public class NoticeQueryService {
   public Page<NoticeSimpleResponse> getAllNotices(Pageable pageable) {
     return noticeRepository.findAllNotice(pageable)
             .map(NoticeSimpleResponse::fromEntity);
+  }
+
+  public NoticeDetailResponse getNoticeDetail(Long noticeId) {
+    return noticeRepository.findNoticeById(noticeId)
+            .map(NoticeDetailResponse::fromEntity)
+            .orElseThrow(() -> new EntityNotFoundException("공지사항을 찾을 수 없습니다. id=" + noticeId));
   }
 }
