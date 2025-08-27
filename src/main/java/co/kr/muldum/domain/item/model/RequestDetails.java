ab470b9139c849bb39533931427a16eda7309e38
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-08-27 12:26:53 +0900

    feat: 물품정보 json 추가

---
+++ src/main/java/co/kr/muldum/domain/item/model/RequestDetails.java
@@ -0,0 +1,16 @@
+package co.kr.muldum.domain.item.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestDetails {
+    private String reason;
+    private String deliveryInfo;
+    private String approvalNotes;
+}
