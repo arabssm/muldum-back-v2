6424c5934734553f4dff61d9eaada727a832d694
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-08-27 14:26:51 +0900

    refactor: 사용자 인증 추가

--- src/main/java/co/kr/muldum/presentation/item/ItemController.java
+++ src/main/java/co/kr/muldum/presentation/item/ItemController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/std/items")
 @Slf4j
+@PreAuthorize("hasRole('STUDENT')")
 public class ItemController {
 
     private final ItemRequestService itemRequestService;
