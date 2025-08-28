b57936e59b4d8a5827b908f261ac2e3b6e66f87a
randirao <randirao4245@gmail.com>
2025-08-28 19:20:00 +0900

    chore: 불필요한 어노테이션 제거

--- src/main/java/co/kr/muldum/presentation/dto/TeamSheetImportRequestDto.java
+++ src/main/java/co/kr/muldum/presentation/dto/TeamSheetImportRequestDto.java
@@ -1,19 +1,10 @@
 package co.kr.muldum.presentation.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-/**
- * Request payload for sheet import API.
- */
 @Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class TeamSheetImportRequestDto {
     private String sheetLink;
     private String range;
