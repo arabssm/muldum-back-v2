bb9aeb2d9cf9eb5f4cdf5738fb8da6256284d8ad
randirao <randirao4245@gmail.com>
2025-08-29 22:25:08 +0900

    feat: 로그아웃 요청 DTO에 @NotBlank 검증 추가

--- src/main/java/co/kr/muldum/presentation/dto/LogoutRequestDto.java
+++ src/main/java/co/kr/muldum/presentation/dto/LogoutRequestDto.java
@@ -2,10 +2,12 @@ package co.kr.muldum.presentation.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @AllArgsConstructor
 public class LogoutRequestDto {
 
+    @NotBlank
     private String refreshToken;
 }
