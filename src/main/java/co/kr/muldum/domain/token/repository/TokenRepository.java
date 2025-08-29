9cd231301c13c3c9a27f0cb7c3fbd74b0150b334
randirao <randirao4245@gmail.com>
2025-08-29 21:57:08 +0900

    fix: 반환 타입을 Optional로 감쌈

--- src/main/java/co/kr/muldum/domain/token/repository/TokenRepository.java
+++ src/main/java/co/kr/muldum/domain/token/repository/TokenRepository.java
@@ -3,7 +3,9 @@ package co.kr.muldum.domain.token.repository;
 import co.kr.muldum.domain.token.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TokenRepository extends JpaRepository<RefreshToken, String> {
 
-    long deleteByRefreshToken(String refreshToken);
+    Optional<RefreshToken> deleteByRefreshToken(String token);
 }
