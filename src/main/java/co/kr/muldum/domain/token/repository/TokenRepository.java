77c14c802c51362585cb23f59edb182a14f4428b
randirao <randirao4245@gmail.com>
2025-08-29 14:42:37 +0900

    fix: deleteByToken 메서드 추가로 로그아웃 로직 단순화

--- src/main/java/co/kr/muldum/domain/token/repository/TokenRepository.java
+++ src/main/java/co/kr/muldum/domain/token/repository/TokenRepository.java
@@ -3,6 +3,7 @@ package co.kr.muldum.domain.token.repository;
 import co.kr.muldum.domain.token.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public  interface TokenRepository extends JpaRepository<RefreshToken, String> {
+public interface TokenRepository extends JpaRepository<RefreshToken, String> {
 
+    boolean deleteByToken(String token);
 }
