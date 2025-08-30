bfb941e1d999e52b3532c742ffcde05a842aca40
randirao <randirao4245@gmail.com>
2025-08-30 19:00:03 +0900

    feat: 팀스페이스 초대 요청 DTO 구현

--- src/main/java/co/kr/muldum/presentation/teamspace/dto/TeamspaceInviteRequestDto.java
+++ src/main/java/co/kr/muldum/presentation/teamspace/dto/TeamspaceInviteRequestDto.java
@@ -3,6 +3,7 @@ package co.kr.muldum.presentation.teamspace.dto;
 import java.util.List;
 
 public record TeamspaceInviteRequestDto(
+
         List<EmailRequest> emails
 ) {
     public record EmailRequest(String email) {}
