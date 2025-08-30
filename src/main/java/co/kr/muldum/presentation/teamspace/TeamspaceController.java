d828bc759c65d0c7afe1abd87997fff8dbc0bd2e
randirao <randirao4245@gmail.com>
2025-08-30 22:30:23 +0900

    fix: TeamspaceController에 학생 전용 권한 제어 추가

--- src/main/java/co/kr/muldum/presentation/teamspace/TeamspaceController.java
+++ src/main/java/co/kr/muldum/presentation/teamspace/TeamspaceController.java
@@ -6,10 +6,12 @@ import co.kr.muldum.presentation.teamspace.dto.TeamspaceInviteResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
 @RequestMapping("/std/team")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('STUDENT')")
 public class TeamspaceController {
 
     private final TeamspaceService teamspaceService;
