789459f543030839053a855093db142ae6416a9b
randirao <randirao4245@gmail.com>
2025-08-31 00:38:12 +0900

    feat: UserRepository 생성

---
+++ src/main/java/co/kr/muldum/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package co.kr.muldum.domain.user.repository;
+
+import co.kr.muldum.domain.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
