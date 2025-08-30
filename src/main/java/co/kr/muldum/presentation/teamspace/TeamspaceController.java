2620de8c56a2c44f08459979e5ac5d0dc32b8d40
randirao <randirao4245@gmail.com>
2025-08-30 18:57:15 +0900

    refactor: 컨트롤러 요청 DTO를 StudentCsvImportRequestDto로 통일

--- src/main/java/co/kr/muldum/presentation/teamspace/TeamspaceController.java
+++ src/main/java/co/kr/muldum/presentation/teamspace/TeamspaceController.java
@@ -1,8 +1,8 @@
 package co.kr.muldum.presentation.teamspace;
 
-import co.kr.muldum.presentation.teamspace.dto.TeamspaceInviteRequestDto;
-import co.kr.muldum.presentation.teamspace.dto.TeamspaceInviteResponse;
+import co.kr.muldum.application.teamspace.StudentCsvImportRequest;
 import co.kr.muldum.application.teamspace.TeamspaceService;
+import co.kr.muldum.presentation.teamspace.dto.TeamspaceInviteResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +15,10 @@ public class TeamspaceController {
     private final TeamspaceService teamspaceService;
 
     @PostMapping("/invite")
-    public ResponseEntity<TeamspaceInviteResponse> inviteStudents(
-            @RequestBody TeamspaceInviteRequestDto request
+    public ResponseEntity<TeamspaceInviteResponseDto> inviteStudents(
+            @RequestBody StudentCsvImportRequest request
     ) {
-        TeamspaceInviteResponse response = teamspaceService.inviteStudents(request);
+        TeamspaceInviteResponseDto response = teamspaceService.inviteStudents(request);
         return ResponseEntity.status(201).body(response);
     }
 }
