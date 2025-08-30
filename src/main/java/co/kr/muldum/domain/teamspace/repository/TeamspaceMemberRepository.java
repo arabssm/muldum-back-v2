41cfa76433e7a7c3d49a49c47ba075155283b7b8
randirao <randirao4245@gmail.com>
2025-08-30 23:36:50 +0900

    feat: TeamspaceMemberRepository 구현

---
+++ src/main/java/co/kr/muldum/domain/teamspace/repository/TeamspaceMemberRepository.java
@@ -0,0 +1,10 @@
+package co.kr.muldum.domain.teamspace.repository;
+
+import co.kr.muldum.domain.teamspace.model.Team;
+import co.kr.muldum.domain.teamspace.model.TeamspaceMember;
+import co.kr.muldum.domain.user.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamspaceMemberRepository extends JpaRepository<TeamspaceMember, Long> {
+    boolean existsByTeamAndStudent(Team team, Student student);
+}
