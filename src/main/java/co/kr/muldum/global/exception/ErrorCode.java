9b259f891b184023ac3ddeed4c86dcc258f71b83
randirao <randirao4245@gmail.com>
2025-08-31 21:02:28 +0900

    feat: ErrorCode에 팀스페이스 초대 관련 코드 추가

--- src/main/java/co/kr/muldum/global/exception/ErrorCode.java
+++ src/main/java/co/kr/muldum/global/exception/ErrorCode.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INVALID_AUTH_CODE(HttpStatus.UNAUTHORIZED, "잘못된 인증 코드입니다."),
     UNAUTHORIZED_DOMAIN(HttpStatus.UNAUTHORIZED, "허용되지 않은 이메일 도메인입니다."),
-    UNREGISTERED_USER(HttpStatus.UNAUTHORIZED, "등록되지 않은 사용자입니다.");
+    UNREGISTERED_USER(HttpStatus.UNAUTHORIZED, "등록되지 않은 사용자입니다."),
+    INVALID_GOOGLE_SHEET_URL(HttpStatus.BAD_REQUEST, "잘못된 구글 시트 URL입니다."),
+    TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "팀을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
