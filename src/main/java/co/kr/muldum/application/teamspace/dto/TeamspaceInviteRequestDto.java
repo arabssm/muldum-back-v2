b4080eba27347de25858cb7c3d43b5e96efd7171
randirao <randirao4245@gmail.com>
2025-08-31 20:32:17 +0900

    feat: teamspace 요청 dto 구현

---
+++ src/main/java/co/kr/muldum/application/teamspace/dto/TeamspaceInviteRequestDto.java
@@ -0,0 +1,11 @@
+package co.kr.muldum.application.teamspace.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TeamspaceInviteRequestDto {
+
+    private String googleSheetUrl;
+}
