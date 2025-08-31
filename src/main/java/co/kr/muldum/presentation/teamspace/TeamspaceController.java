46463ce7a45b0a3ad63795bde21c71f65e0c97ac
randirao <randirao4245@gmail.com>
2025-09-01 01:04:08 +0900

    fix: 교사만 팀 스페이스 추가 가능하도록 권한 설정

--- src/main/java/co/kr/muldum/presentation/teamspace/TeamspaceController.java
+++ src/main/java/co/kr/muldum/presentation/teamspace/TeamspaceController.java
@@ -5,11 +5,13 @@ import co.kr.muldum.application.teamspace.dto.TeamspaceInviteRequestDto;
 import co.kr.muldum.application.teamspace.dto.TeamspaceInviteResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/std/team")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('TEACHER')")
 public class TeamspaceController {
 
     private final TeamspaceService teamspaceService;
