cbddee1761f947ac93fe68301bbcc91b0eeb0776
randirao <randirao4245@gmail.com>
2025-08-31 20:55:54 +0900

    feat: 팀 스페이스 초대 컨트롤러 로직 구현

---
+++ src/main/java/co/kr/muldum/presentation/teamspace/TeamspaceController.java
@@ -0,0 +1,24 @@
+package co.kr.muldum.presentation.teamspace;
+
+import co.kr.muldum.application.teamspace.TeamspaceService;
+import co.kr.muldum.application.teamspace.dto.TeamspaceInviteRequestDto;
+import co.kr.muldum.application.teamspace.dto.TeamspaceInviteResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/std/team")
+@RequiredArgsConstructor
+public class TeamspaceController {
+
+    private final TeamspaceService teamspaceService;
+
+    @PostMapping("/invite")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TeamspaceInviteResponseDto inviteStudents(
+            @RequestParam Long teamId,
+            @RequestBody TeamspaceInviteRequestDto requestDto) {
+        return teamspaceService.inviteStudents(teamId, requestDto);
+    }
+}
