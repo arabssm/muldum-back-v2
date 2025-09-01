800315156e5e37850aba8a3de5513ef443f47908
randirao <randirao4245@gmail.com>
2025-09-01 17:00:28 +0900

    feat: studentNumber 기반 사용자 조회 메서드 추가

--- src/main/java/co/kr/muldum/domain/user/repository/UserRepository.java
+++ src/main/java/co/kr/muldum/domain/user/repository/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findByEmail(String email);
+
+    // studentNumber로 유저 조회
+    Optional<User> findByStudentNumber(String studentNumber);
 }
