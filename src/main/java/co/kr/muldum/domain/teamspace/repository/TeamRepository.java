10c8fb209b8aa86fe69eb66cfb2099efcaf48986
randirao <randirao4245@gmail.com>
2025-09-01 22:03:56 +0900

    feat: 팀 조회를 위한 findByName, findMaxId 메서드 추가

--- src/main/java/co/kr/muldum/domain/teamspace/repository/TeamRepository.java
+++ src/main/java/co/kr/muldum/domain/teamspace/repository/TeamRepository.java
@@ -2,10 +2,15 @@ package co.kr.muldum.domain.teamspace.repository;
 
 import co.kr.muldum.domain.teamspace.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
     Optional<Team> findByName(String name);
+
+    // 팀의 최대 id 조회
+    @Query("SELECT MAX(t.id) FROM Team t")
+    Optional<Long> findMaxId();
 }
