d7f7d4110ebfe9c6f480a183de4131332fa100b6
123isi <123isi@naver.com>
2025-09-02 01:29:19 +0900

    fix :: refresh

--- src/main/java/co/kr/muldum/application/auth/RefreshResponse.java
+++ src/main/java/co/kr/muldum/application/auth/RefreshResponse.java
@@ -1,9 +1,11 @@
 package co.kr.muldum.application.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class RefreshResponse {
   private final String access_token;
 
-  public RefreshResponse(String access_token) {
-    this.access_token = access_token;
-  }
 }
