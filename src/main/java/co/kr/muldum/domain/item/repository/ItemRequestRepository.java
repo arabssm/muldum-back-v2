84f9a6aabe34fca888d64e2c31141f7343b73226
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-09-02 00:16:27 +0900

    hotfix: 잘못된 의존성 주입 수정

--- src/main/java/co/kr/muldum/domain/item/repository/ItemRequestRepository.java
+++ src/main/java/co/kr/muldum/domain/item/repository/ItemRequestRepository.java
@@ -10,10 +10,7 @@ import java.util.List;
 @Repository
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
     List<ItemRequest> findByTeamIdAndStatus(Integer teamId, ItemStatus itemStatus);
-
     List<ItemRequest> findByTeamId(Integer teamId);
-    List<ItemRequest> findByStatusIn(List<ItemStatus> statuses);
-    List<ItemRequest> findByTeamIdAndStatusIn(Integer teamId, List<ItemStatus> statuses);
     List<ItemRequest> findByStatus(ItemStatus itemStatus);
     List<ItemRequest> findByRequesterUserIdAndStatus(Integer requesterUserId, ItemStatus itemStatus);
     List<ItemRequest> findByTeamIdAndStatusNot(Integer teamId, ItemStatus excludeStatus);
