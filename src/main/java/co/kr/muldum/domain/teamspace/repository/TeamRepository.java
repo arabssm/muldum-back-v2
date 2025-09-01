bf1c307358e51bdda9f4e39f6f14b0f170bd46fc
randirao <randirao4245@gmail.com>
2025-09-01 17:44:31 +0900

    feat: 팀 이름으로 조회할 수 있는 findByName 메서드 추가

--- src/main/java/co/kr/muldum/domain/teamspace/repository/TeamRepository.java
+++ src/main/java/co/kr/muldum/domain/teamspace/repository/TeamRepository.java
@@ -3,5 +3,9 @@ package co.kr.muldum.domain.teamspace.repository;
 import co.kr.muldum.domain.teamspace.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Optional<Team> findByName(String name);
 }
