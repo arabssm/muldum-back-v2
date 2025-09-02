8166850e859665d6915f3f7199a4cf5cba692f93
randirao <randirao4245@gmail.com>
2025-09-02 19:21:02 +0900

    feat: 팀과 사용자 찾는 메서드 추가

--- src/main/java/co/kr/muldum/domain/teamspace/repository/TeamspaceMemberRepository.java
+++ src/main/java/co/kr/muldum/domain/teamspace/repository/TeamspaceMemberRepository.java
@@ -4,9 +4,12 @@ import co.kr.muldum.domain.teamspace.model.Team;
 import co.kr.muldum.domain.teamspace.model.TeamspaceMember;
 import co.kr.muldum.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface TeamspaceMemberRepository extends JpaRepository<TeamspaceMember, Long> {
 
     // 팀과 유저 기준으로 멤버가 이미 존재하는지 확인
     boolean existsByTeamAndUser(Team team, User user);
+
+    Optional<TeamspaceMember> findByTeamAndUser(Team team, User user);
 }
