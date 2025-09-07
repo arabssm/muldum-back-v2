b76aa9c2032abc172685e70d61d13a37bcc5d3b5
randirao <randirao4245@gmail.com>
2025-09-07 20:44:32 +0900

    fix: NOT_FOUND 에러 메시지 수정

--- src/main/java/co/kr/muldum/global/exception/ErrorCode.java
+++ src/main/java/co/kr/muldum/global/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     TEAM_NOT_FOUND(HttpStatus.NOT_FOUND, "팀을 찾을 수 없습니다."),
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     INVALID_ROLE(HttpStatus.BAD_REQUEST, "잘못된 역할입니다."),
-    NOT_FOUND_COLUMN(HttpStatus.NOT_FOUND, "컬럼이 일치하지 않거나 찾을 수 없습니다.");
+    NOT_FOUND_COLUMN(HttpStatus.NOT_FOUND, "컬럼이 일치하지 않거나 찾을 수 없습니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다.");
 
   private final HttpStatus status;
     private final String message;
