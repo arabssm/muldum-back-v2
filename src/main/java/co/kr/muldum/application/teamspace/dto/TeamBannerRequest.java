c74d17b36d2de9de6a6427c48a7c3de459cf45c0
심현진 <hyunjin.dev143@gmail.com>
2025-09-07 23:53:46 +0900

    feat: 팀 배너 요청을 위한 DTO 클래스 추가

---
+++ src/main/java/co/kr/muldum/application/teamspace/dto/TeamBannerRequest.java
@@ -0,0 +1,11 @@
+package co.kr.muldum.application.teamspace.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class TeamBannerRequest {
+
+  @NotBlank(message = "파일 url은 필수입니다.")
+  private String url;
+}
