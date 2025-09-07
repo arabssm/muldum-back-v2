7e4f0b56a73a6db1b954bcf903b8ba0a3312485a
randirao <randirao4245@gmail.com>
2025-09-07 21:39:07 +0900

    feat: 팀 페이지 조회 API 컨트롤러에 접근 제어(@PreAuthorize) 추가

--- src/main/java/co/kr/muldum/presentation/teamspace/TeamPageController.java
+++ src/main/java/co/kr/muldum/presentation/teamspace/TeamPageController.java
@@ -4,6 +4,7 @@ import co.kr.muldum.application.teamspace.TeamPageQueryService;
 import co.kr.muldum.application.teamspace.dto.TeamPageQueryResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class TeamPageController {
     private final TeamPageQueryService teamPageQueryService;
 
     // 팀 페이지 조회
+    @PreAuthorize("@teamspaceAuth.hasMembership(authentication, #teamId)")
     @GetMapping("/{teamId}")
     public ResponseEntity<TeamPageQueryResponseDto> getTeamPage(@PathVariable Long teamId) {
         // 조회만
