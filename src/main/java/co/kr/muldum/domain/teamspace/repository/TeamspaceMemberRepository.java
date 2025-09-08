e61feb29be32a87bf87a3d83cd6fda0dfc73f7a4
심현진 <hyunjin.dev143@gmail.com>
2025-09-09 08:48:28 +0900

    feat: 팀 멤버 존재 여부 확인을 위한 쿼리 메서드 추가

--- src/main/java/co/kr/muldum/domain/teamspace/repository/TeamspaceMemberRepository.java
+++ src/main/java/co/kr/muldum/domain/teamspace/repository/TeamspaceMemberRepository.java
@@ -1,25 +1,28 @@
 package co.kr.muldum.domain.teamspace.repository;
 
 import co.kr.muldum.domain.teamspace.model.Team;
-import co.kr.muldum.domain.teamspace.model.TeamType;
 import co.kr.muldum.domain.teamspace.model.TeamspaceMember;
 import co.kr.muldum.domain.user.model.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TeamspaceMemberRepository extends JpaRepository<TeamspaceMember, Long> {
 
     // 팀과 유저 기준으로 멤버가 이미 존재하는지 확인
     boolean existsByTeamAndUser(Team team, User user);
 
-    Optional<TeamspaceMember> findByTeamAndUser(Team team, User user);
-
     //팀 조회
     @EntityGraph(attributePaths = "user")
     List<TeamspaceMember> findByTeam(Team team);
 
-    List<Team> findDistinctByUserAndTeam_Type(User user, TeamType teamType);
+    @Query("""
+      select count(tm) > 0
+      from TeamspaceMember tm
+      where tm.team.id = :teamId
+      and tm.user.id = :userId
+    """)
+    boolean existsByTeamIdAndUserId(Long teamId, Long userId);
 }
