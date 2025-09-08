409edc88641e64fa818164b1df479f5df72d7f86
심현진 <hyunjin.dev143@gmail.com>
2025-09-09 08:42:58 +0900

    fix: 팀 배너 업로드 API 경로 수정 및 사용자 인증 정보 추가

--- src/main/java/co/kr/muldum/presentation/teamspace/FileUploadController.java
+++ src/main/java/co/kr/muldum/presentation/teamspace/FileUploadController.java
@@ -2,8 +2,10 @@ package co.kr.muldum.presentation.teamspace;
 
 import co.kr.muldum.application.teamspace.FileUploadService;
 import co.kr.muldum.application.teamspace.dto.TeamBannerRequest;
+import co.kr.muldum.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,13 +15,13 @@ public class FileUploadController {
 
   private final FileUploadService fileUploadService;
 
-  @PatchMapping("/network/team/{teamId}")
+  @PatchMapping("/network/team/{teamId}/banner")
   public ResponseEntity<String> uploadTeamNetwork(
           @PathVariable Long teamId,
           @RequestBody TeamBannerRequest teamBannerRequest,
-          @RequestParam(name = "type", defaultValue = "BANNER") String type
+          @AuthenticationPrincipal CustomUserDetails customUserDetails
   ) {
-    fileUploadService.uploadTeamFile(teamId, teamBannerRequest, type);
+    fileUploadService.uploadTeamFile(teamId, teamBannerRequest, customUserDetails.getUserId());
     return ResponseEntity.ok("파일 업로드 성공");
   }
 }
