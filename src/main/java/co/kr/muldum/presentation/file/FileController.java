f7bd850c75cae726ec73364d97df6efc7a9a8f6c
심현진 <hyunjin.dev143@gmail.com>
2025-09-09 17:25:11 +0900

    fix: 파일 경로 요청 엔드포인트에 /ara 제거

--- src/main/java/co/kr/muldum/presentation/file/FileController.java
+++ src/main/java/co/kr/muldum/presentation/file/FileController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("ara/files")
+@RequestMapping("files")
 public class FileController {
 
   private final FileStorageService fileStorageService;
