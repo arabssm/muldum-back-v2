b91a6d639f77e9abb720ce127ee868b60452f92d
randirao <randirao4245@gmail.com>
2025-09-09 20:08:41 +0900

    refactor: 인증 및 엔티티 구조 개선

---
+++ src/main/java/co/kr/muldum/domain/teamspace/model/TeamSettings.java
@@ -0,0 +1,19 @@
+package co.kr.muldum.domain.teamspace.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeamSettings {
+
+    private String theme;
+    private boolean notificationsEnabled;
+    private String language;
+    private int maxMembers;
+    
+}
