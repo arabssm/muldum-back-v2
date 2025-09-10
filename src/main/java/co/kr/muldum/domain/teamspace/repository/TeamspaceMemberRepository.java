85dc73ba959c1e3a8e20bcf71339b1ca5da02ca6
randirao <randirao4245@gmail.com>
2025-09-10 09:37:13 +0900

    fix: 필요 없는 로직 제거

--- src/main/java/co/kr/muldum/domain/teamspace/repository/TeamspaceMemberRepository.java
+++ src/main/java/co/kr/muldum/domain/teamspace/repository/TeamspaceMemberRepository.java
@@ -2,6 +2,7 @@ package co.kr.muldum.domain.teamspace.repository;
 
 import co.kr.muldum.domain.teamspace.model.Team;
 import co.kr.muldum.domain.teamspace.model.TeamspaceMember;
+import co.kr.muldum.domain.teamspace.model.TeamType;
 import co.kr.muldum.domain.user.model.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,20 +13,14 @@ import java.util.Optional;
 
 public interface TeamspaceMemberRepository extends JpaRepository<TeamspaceMember, Long> {
 
-    // 팀과 유저 기준으로 멤버가 이미 존재하는지 확인
-    boolean existsByTeamAndUser(Team team, User user);
-
-    //팀 조회
-    @EntityGraph(attributePaths = "user")
-    List<TeamspaceMember> findByTeam(Team team);
-
-    List<Team> findDistinctByUserAndTeam_Type(User user, TeamType teamType);
     boolean existsByTeamIdAndUserId(Long teamId, Long userId);
-
+    
     boolean existsByTeamAndUser(Team team, User user);
 
     Optional<TeamspaceMember> findByTeamAndUser(Team team, User user);
 
     @EntityGraph(attributePaths = "user")
     List<TeamspaceMember> findByTeam(Team team);
+
+    List<Team> findDistinctByUserAndTeamType(User user, TeamType teamType);
 }
