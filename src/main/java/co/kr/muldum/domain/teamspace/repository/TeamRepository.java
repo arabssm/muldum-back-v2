88f55874b2b8973ab45fcf340078ffd1df9064dc
randirao <randirao4245@gmail.com>
2025-09-11 17:32:40 +0900

    fix: 네트워크 팀 조회 id 순으로 정렬

--- src/main/java/co/kr/muldum/domain/teamspace/repository/TeamRepository.java
+++ src/main/java/co/kr/muldum/domain/teamspace/repository/TeamRepository.java
@@ -12,10 +12,7 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
 
     Optional<Team> findByName(String name);
 
-    // 팀의 최대 id 조회
-    @Query("SELECT MAX(t.id) FROM Team t")
-    Optional<Long> findMaxId();
-
     // 팀 타입으로 팀 목록 조회
+    @Query("SELECT t FROM Team t WHERE t.type = ?1 ORDER BY t.id")
     List<Team> findByType(TeamType type);
 }
