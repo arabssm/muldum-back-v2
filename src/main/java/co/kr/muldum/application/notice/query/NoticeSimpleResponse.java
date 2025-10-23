de26a1f501227634794474a55721afc1fa2d3472
123isi <123isi@naver.com>
2025-10-23 13:55:10 +0900

    refactor :: 공지 조회할때 데드라인도 조회가능하게 수정

--- src/main/java/co/kr/muldum/application/notice/query/NoticeSimpleResponse.java
+++ src/main/java/co/kr/muldum/application/notice/query/NoticeSimpleResponse.java
@@ -2,20 +2,23 @@ package co.kr.muldum.application.notice.query;
 
 import co.kr.muldum.domain.notice.model.Notice;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record NoticeSimpleResponse(
         Long id,
         String title,
         LocalDateTime updatedAt,
-        String teacher
+        String teacher,
+        LocalDate deadlineDate
 ) {
   public static NoticeSimpleResponse fromEntity(Notice notice) {
     return new NoticeSimpleResponse(
             notice.getId(),
             notice.getTitle(),
             notice.getUpdatedAt(),
-            notice.getUser().getName()
+            notice.getUser().getName(),
+            notice.getDeadlineDate()
     );
   }
 }
