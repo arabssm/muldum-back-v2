a7efa43c4d98ec661f0c3ff5ef0a6e2cc005ec5f
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-10-26 22:47:57 +0900

    chore: cloudwatch용 로그 생성

--- src/main/java/co/kr/muldum/MuldumApplication.java
+++ src/main/java/co/kr/muldum/MuldumApplication.java
@@ -2,12 +2,12 @@ package co.kr.muldum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class MuldumApplication {
 
 	public static void main(String[] args) {
