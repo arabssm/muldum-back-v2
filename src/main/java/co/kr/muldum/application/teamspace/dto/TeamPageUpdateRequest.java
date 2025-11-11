50332cc594c752ad629aaddf3cef0d3bbe6ec564
hxeonxins <hyunjin.dev143@gmail.com>
2025-11-11 10:51:54 +0900

    hotfix: 팀 수정 요청 teamName 필드에 not blank 추가

--- src/main/java/co/kr/muldum/application/teamspace/dto/TeamPageUpdateRequest.java
+++ src/main/java/co/kr/muldum/application/teamspace/dto/TeamPageUpdateRequest.java
@@ -3,6 +3,6 @@ package co.kr.muldum.application.teamspace.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record TeamPageUpdateRequest(
-        String teamName,
+        @NotBlank String teamName,
         @NotBlank String content
 ) {}
