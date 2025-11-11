4b6731fee481e5ceb8b0d382352ed93e34ccf0ac
hxeonxins <hyunjin.dev143@gmail.com>
2025-11-11 11:46:59 +0900

    hotfix: 팀 수정 요청에 teamName 필드 name으로 수정

--- src/main/java/co/kr/muldum/application/teamspace/dto/TeamPageUpdateRequest.java
+++ src/main/java/co/kr/muldum/application/teamspace/dto/TeamPageUpdateRequest.java
@@ -3,6 +3,6 @@ package co.kr.muldum.application.teamspace.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record TeamPageUpdateRequest(
-        String teamName,
+        String name,
         @NotBlank String content
 ) {}
