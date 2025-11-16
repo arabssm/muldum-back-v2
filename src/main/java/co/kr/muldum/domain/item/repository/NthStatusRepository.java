6a39467e717301713051245d454588cd34c41b04
hxeonxins <hyunjin.dev143@gmail.com>
2025-11-17 00:30:22 +0900

    feat: NthStatusRepository에 NthStatus ID로 조회하는 쿼리 메서드 추가

--- src/main/java/co/kr/muldum/domain/item/repository/NthStatusRepository.java
+++ src/main/java/co/kr/muldum/domain/item/repository/NthStatusRepository.java
@@ -2,7 +2,13 @@ package co.kr.muldum.domain.item.repository;
 
 import co.kr.muldum.domain.item.model.NthStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface NthStatusRepository extends JpaRepository<NthStatus, Long> {
     NthStatus findNthStatusById(Long id);
+
+    @Query("SELECT ns FROM NthStatus ns WHERE ns.id = :id")
+    Optional<NthStatus> findByNthStatusId(Long id);
 }
