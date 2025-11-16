a73efef6713db96845acc1a9c8ac421e751abc4c
hxeonxins <hyunjin.dev143@gmail.com>
2025-11-16 23:44:04 +0900

    feat: NthStatus에 projectType, guide, deadlineDate, teacherId 필드 추가 및 updateNthValue 메서드 수정

--- src/main/java/co/kr/muldum/domain/item/model/NthStatus.java
+++ src/main/java/co/kr/muldum/domain/item/model/NthStatus.java
@@ -1,10 +1,15 @@
 package co.kr.muldum.domain.item.model;
 
+import co.kr.muldum.domain.item.dto.req.ItemGuide;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.List;
 
 @Entity
 @Table(name = "nth_statuses")
@@ -21,8 +26,27 @@ public class NthStatus {
     @Column(name = "nth_value", nullable = false)
     private Integer nthValue = 0;
 
-    public Integer updateNthValue(Integer nth) {
+    private String projectType;
+
+    @Column(columnDefinition = "jsonb")
+    @JdbcTypeCode(SqlTypes.JSON)
+    private List<ItemGuide> guide;
+
+    private String deadlineDate;
+
+    private Long teacherId;
+
+    public void updateNthValue(
+            Integer nth,
+            String projectType,
+            List<ItemGuide> guide,
+            String deadlineDate,
+            Long teacherId
+    ) {
         this.nthValue = nth;
-        return this.nthValue;
+        this.projectType = projectType;
+        this.guide = guide;
+        this.deadlineDate = deadlineDate;
+        this.teacherId = teacherId;
     }
 }
