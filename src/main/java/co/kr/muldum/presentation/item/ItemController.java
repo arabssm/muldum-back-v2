fa094c64faa459f3b19a2911c0d00d25a2b540b7
hxeonxins <hyunjin.dev143@gmail.com>
2025-11-20 17:36:36 +0900

    feat: Add endpoint to retrieve item guide by guide ID and project type

--- src/main/java/co/kr/muldum/presentation/item/ItemController.java
+++ src/main/java/co/kr/muldum/presentation/item/ItemController.java
@@ -1,11 +1,12 @@
 package co.kr.muldum.presentation.item;
 
+import co.kr.muldum.domain.item.dto.res.GetItemGuideResponse;
+import co.kr.muldum.domain.item.service.ItemRequestService;
 import co.kr.muldum.domain.item.service.TeacherItemService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemController {
 
     private final TeacherItemService teacherItemService;
+    private final ItemRequestService itemRequestService;
 
     @PostMapping("/items/issue")
     public ResponseEntity<String> fixNthIssues() {
@@ -21,5 +23,17 @@ public class ItemController {
         String response = teacherItemService.fixNthIssues();
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/ara/items/guide/{guide_id}")
+    public ResponseEntity<GetItemGuideResponse> getOneItemGuide(
+            @PathVariable("guide_id") Long guideId,
+            @RequestParam("type") String projectType
+    ) {
+        log.info("물품 가이드 조회 요청 - guideId: {}, projectType: {}", guideId, projectType);
+
+        GetItemGuideResponse itemGuide = itemRequestService.getItemGuide(guideId, projectType);
+
+        return ResponseEntity.ok(itemGuide);
     }
 }
