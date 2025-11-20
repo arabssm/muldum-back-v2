921769373add5ba721b71c154c478c71951ef85f
hxeonxins <hyunjin.dev143@gmail.com>
2025-11-20 16:05:03 +0900

    feat: Enhance ItemGuideRequest and ItemGuideResponse DTOs with additional fields and Lombok annotations

--- src/main/java/co/kr/muldum/domain/item/dto/req/ItemGuideRequest.java
+++ src/main/java/co/kr/muldum/domain/item/dto/req/ItemGuideRequest.java
@@ -1,5 +1,9 @@
 package co.kr.muldum.domain.item.dto.req;
 
+import lombok.Getter;
+
+@Getter
 public class ItemGuideRequest {
     private String content;
+    private String projectType;
 }
--- src/main/java/co/kr/muldum/domain/item/dto/res/ItemGuideResponse.java
+++ src/main/java/co/kr/muldum/domain/item/dto/res/ItemGuideResponse.java
@@ -1,5 +1,10 @@
 package co.kr.muldum.domain.item.dto.res;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class ItemGuideResponse {
     private Long id;
     private String message;
