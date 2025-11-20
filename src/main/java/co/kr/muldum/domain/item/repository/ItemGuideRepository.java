b80678584d758baa04bc88e902a440423edae3f4
hxeonxins <hyunjin.dev143@gmail.com>
2025-11-20 16:04:51 +0900

    feat: Create ItemGuideRepository interface for ItemGuide entity with custom query method

---
+++ src/main/java/co/kr/muldum/domain/item/repository/ItemGuideRepository.java
@@ -0,0 +1,12 @@
+package co.kr.muldum.domain.item.repository;
+
+import co.kr.muldum.domain.item.model.ItemGuide;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ItemGuideRepository extends JpaRepository<ItemGuide, Long> {
+    Optional<ItemGuide> findByIdAndTeacherId(Long id, Long teacherId);
+}
