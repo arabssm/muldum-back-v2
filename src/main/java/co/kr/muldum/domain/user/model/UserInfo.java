dd22303979666f45a01b6f59018976dcb389fd29
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2025-11-24 14:55:12 +0900

    refactor: 임포트형식으로 수정

--- src/main/java/co/kr/muldum/domain/user/model/UserInfo.java
+++ src/main/java/co/kr/muldum/domain/user/model/UserInfo.java
@@ -2,13 +2,14 @@ package co.kr.muldum.domain.user.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import java.util.List;
 
 @Getter
 @Builder
 public class UserInfo {
     private Long userId;
     private String name;
-    private java.util.List<Long> teamIds;
+    private List<Long> teamIds;
     private Role role;
     private UserType userType;
 }
